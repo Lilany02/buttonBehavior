afa65f6770e80411548a19d7aaecc5f4cd081550
Lilany02 <lilany@gmail.com>
2020-10-29 16:24:16 +0100

    modifications

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const buttonOne = document.getElementById('button-one').addEventListener('click'
 const buttonTwo = document.getElementById('button-two').addEventListener('click', function changeBackgroundColor() {
     document.querySelector('.logo').style.backgroundColor = 'red'} ) 
 const buttonThree = document.getElementById('button-three').addEventListener('click', function addShadow() {
-    document.querySelector('.logo').style.boxShadow = '30px 10px 4px #333'})
+    document.querySelector('.logo').style.boxShadow = '30px 20px 4px #999'})
 
     //resetting the original state 
+
+/*
+    const button = document.getElementsByClassName('button-container').addEventListener('dblclick', function doubleClick(){
+        document.querySelector('.logo').style.backgroundColor = '#000'
+        document.querySelector('#text').style.color = 'red';}    
+    )
+
+
+    */
